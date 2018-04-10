2d8114af7a2410a55272ddbceb05b3a8bf573260
Timon <mail@weeaboo.nl>
2018-04-10 21:31:42 +0200

    Make 'stack depth warn limit' in ObjectDeserializer configurable

--- core/src/main/java/nl/weeaboo/lua2/io/ObjectDeserializer.java
+++ core/src/main/java/nl/weeaboo/lua2/io/ObjectDeserializer.java
@@ -14,22 +14,21 @@ import org.slf4j.LoggerFactory;
 public class ObjectDeserializer extends ObjectInputStream {
 
     private static final Logger LOG = LoggerFactory.getLogger(ObjectDeserializer.class);
-    private static final int STACK_DEPTH_WARN_LIMIT = 100;
 
-	private final Environment env;
+    private final Environment env;
     private final ExecutorService executor;
 
     private boolean collectStats = true;
-	private int maxDepth = 0;
+    private int depthWarnLimit = 100;
 
     protected ObjectDeserializer(InputStream in, Environment e) throws IOException {
-		super(in);
+        super(in);
 
-		env = (e.size() > 0 ? e : null);
+        env = (e.size() > 0 ? e : null);
         executor = new DelayedIoExecutor("LuaObjectDeserializer");
 
-		updateEnableReplace();
-	}
+        updateEnableReplace();
+    }
 
     @Override
     public void close() throws IOException {
@@ -40,15 +39,15 @@ public class ObjectDeserializer extends ObjectInputStream {
         }
     }
 
-	private void updateEnableReplace() {
+    private void updateEnableReplace() {
         boolean replace = (env != null || collectStats);
 
-		try {
-			enableResolveObject(replace);
-		} catch (SecurityException se) {
+        try {
+            enableResolveObject(replace);
+        } catch (SecurityException se) {
             LOG.error("Error calling 'enableReplaceObject'", se);
-		}
-	}
+        }
+    }
 
     public Object readObjectOnNewThread() throws IOException {
         Future<Object> future = executor.submit(createAsyncReadTask());
@@ -59,7 +58,7 @@ public class ObjectDeserializer extends ObjectInputStream {
         } catch (ExecutionException e) {
             throw new IOException("Error during async read", e.getCause());
         }
-	}
+    }
 
     protected Callable<Object> createAsyncReadTask() {
         return new Callable<Object>() {
@@ -71,7 +70,7 @@ public class ObjectDeserializer extends ObjectInputStream {
     }
 
     @Override
-	protected Object resolveObject(Object obj) throws IOException {
+    protected Object resolveObject(Object obj) throws IOException {
         Class<?> clazz = obj.getClass();
 
         if (collectStats) {
@@ -79,20 +78,20 @@ public class ObjectDeserializer extends ObjectInputStream {
             StackTraceElement[] stackTrace = dummy.getStackTrace();
 
             int depth = stackTrace.length;
-            maxDepth = Math.max(maxDepth, depth);
-            if (depth >= STACK_DEPTH_WARN_LIMIT) {
-                LOG.warn("Max stack depth exceeded ({}) while reading {}", depth, clazz.getName(), dummy);
+            if (depth >= depthWarnLimit) {
+                LOG.warn("Max stack depth exceeded ({}/{}) while reading {}", depth, depthWarnLimit,
+                        clazz.getName(), dummy);
             } else {
                 LOG.trace("Stack depth ({}) while reading {}", depth, clazz);
             }
-		}
+        }
 
-		if (clazz == RefEnvironment.class) {
+        if (clazz == RefEnvironment.class) {
             return ((RefEnvironment)obj).resolve(env);
-		}
+        }
 
-		return obj;
-	}
+        return obj;
+    }
 
     public boolean getCollectStats() {
         return collectStats;
@@ -103,6 +102,14 @@ public class ObjectDeserializer extends ObjectInputStream {
             collectStats = enable;
             updateEnableReplace();
         }
+    }
+
+    public int getDepthWarnLimit() {
+        return depthWarnLimit;
+    }
+
+    public void setDepthWarnLimit(int limit) {
+        depthWarnLimit = limit;
     }
 
 }
